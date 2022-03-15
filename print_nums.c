cc0cf8025c17077e6c9669a6abf02bb4eb40c114
Noluyolo97 <98300986+Noluyolo97@users.noreply.github.com>
2022-03-15 09:18:19 +0200

    Update print_nums.c

--- print_nums.c
+++ print_nums.c
@@ -1,75 +1,131 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * print_int - prints an integer
- * @l: va_list of arguments from _printf
- * @f: pointer to the struct flags determining
- * if a flag is passed to _printf
- * Return: number of char printed
+ * print_unsigned_number - print unsigned number
+ * @n: unsigned int to print
+ * Return: number of printed chars
  */
-int print_int(va_list l, mods *f)
+
+int print_unsigned_number(unsigned int n)
 {
-	int n = va_arg(l, int);
-	int res = count_digit(n);
+	int count = 0;
+	unsigned int nb = n;
 
-	if (f->space && !f->plus && n >= 0)
-		res += _putchar(' ');
-	if (f->plus && n >= 0)
-		res += _putchar('+');
-	if (n <= 0)
-		res++;
-	print_number(n);
-	return (res);
-}
-
-/**
- * print_unsigned - prints an unsigned integer
- * @l: va_list of arguments from _printf
- * @f: pointer to the struct flags determining
- * if a flag is passed to _printf
- * Return: number of char printed
- */
-int print_unsigned(va_list l, mods *f)
-{
-	unsigned int u = va_arg(l, unsigned int);
-	char *str = convert(u, 10, 0);
-
-	(void)f;
-	return (_puts(str));
-}
-
-/**
- * print_number - helper function that loops through
- * an integer and prints all its digits
- * @n: integer to be printed
- */
-void print_number(int n)
-{
-	unsigned int n1;
-
-	if (n < 0)
-		_putchar('-');
-	n1 = ABS(n);
-	if (n1 / 10)
-		print_number(n1 / 10);
-	_putchar((n1 % 10) + '0');
-}
-
-/**
- * count_digit - returns the number of digits in an integer
- * for _printf
- * @i: integer to evaluate
- * Return: number of digits
- */
-int count_digit(int i)
-{
-	unsigned int d = 0;
-	unsigned int u = ABS(i);
-
-	while (u)
+	if (nb <= 9)
 	{
-		u /= 10;
-		d++;
+		_putchar(nb + '0');
+		return (1);
 	}
-	return (d);
+	if (nb > 9)
+	{
+		count = print_unsigned_number(nb / 10) + 1;
+		_putchar(nb % 10 + '0');
+		return (count);
+	}
+	return (0);
+}
+
+/**
+ * print_number - display the number contained in an int
+ * @n: int to print
+ * Return: number of char
+ */
+int print_number(int n)
+{
+	unsigned int nb;
+	int count = 0;
+
+	nb = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		nb = -nb;
+	}
+	if (nb <= 9)
+	{
+		count += _putchar(nb + '0');
+		return (count);
+	}
+	if (nb > 9)
+	{
+		count += print_number(nb / 10) + 1;
+		_putchar(nb % 10 + '0');
+		return (count);
+	}
+	return (0);
+}
+/**
+ * _nbr_len - length of a number
+ * @prmNumber: number
+ * Return: length of the number
+ */
+
+int _nbr_len(int prmNumber)
+{
+	int cLoop = 0, number;
+
+	if (prmNumber == 0)
+		return (1);
+
+	number = prmNumber;
+
+	if (number < 0)
+	{
+		number *= -1;
+		cLoop++;	/* for the sign char */
+	}
+
+	while (number)
+	{
+		number /= 10;
+		cLoop++;
+	}
+
+	return (cLoop);
+}
+
+/**
+ * _itoa - Convert an int to a string
+ * @prmNumber: int to convert
+ * Return: converted string
+ */
+
+char *_itoa(int prmNumber)
+{
+	char *s;
+	int cLoop;
+	long number;
+
+	number = prmNumber;
+	cLoop = _nbr_len(number);
+	s = malloc(sizeof(char) * cLoop + 1);
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	s[cLoop] = '\0';
+
+	if (number == 0)
+	{
+		s = "0";
+	}
+	else if (number < 0)
+	{
+		s[0] = '-';
+		number *= -1;
+	}
+
+	while (number)
+	{
+		s[--cLoop] = number % 10 + 48;
+		number /= 10;
+	}
+
+	return (s);
 }

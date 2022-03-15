82fe5aecfdc1da4c73e0aa86818eda0d8634bcf0
Noluyolo97 <98300986+Noluyolo97@users.noreply.github.com>
2022-03-15 08:43:19 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,49 +1,43 @@
 #include "main.h"
 
 /**
- * _printf - prints anything
- * @format: the format string
- *
- * Return: number of bytes printed
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
-	int sum = 0;
-	va_list ap;
-	char *p, *start;
-	params_t params = PARAMS_INIT;
+	register short len = 0;
+	int (*printFunc)(va_list, mods *);
+	mods prefixes = PF_INIT;
+	const char *p = format;
+	va_list arguments;
 
-	va_start(ap, format);
-
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = (char *)format; *p; p++)
+	va_start(arguments, format);
+	assert(invalidInputs(p));
+	for (; *p; p++)
 	{
-		init_params(&params, ap);
-		if (*p != '%')
+		if (*p == '%')
 		{
-			sum += _putchar(*p);
-			continue;
-		}
-		start = p;
-		p++;
-		while (get_flag(p, &params)) /* while char at p is flag char */
-		{
-			p++; /* next char */
-		}
-		p = get_width(p, &params, ap);
-		p = get_precision(p, &params, ap);
-		if (get_modifier(p, &params))
 			p++;
-		if (!get_specifier(p))
-			sum += print_from_to(start, p,
-				params.l_modifier || params.h_modifier ? p - 1 : 0);
-		else
-			sum += get_print_func(p, ap, &params);
+			if (*p == '%')
+			{
+				len += _putchar('%');
+				continue;
+			}
+			while (get_flags(*p, &prefixes))
+				p++;
+			printFunc = get_print(*p);
+			len += (printFunc)
+				? printFunc(arguments, &prefixes)
+				: _printf("%%%c", *p);
+		} else
+			len += _putchar(*p);
 	}
-	_putchar(BUF_FLUSH);
-	va_end(ap);
-	return (sum);
+	_putchar(FLUSH);
+	va_end(arguments);
+	return (len);
 }
